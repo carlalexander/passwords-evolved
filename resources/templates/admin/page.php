104303969381d8f51ceb5a90bca5f2a17293b611
Cornel Raiu <cornelRaiu@users.noreply.github.com>
2022-04-18 01:59:53 +0300

    Add missing echo on screen reader 'settings_saved'

--- resources/templates/admin/page.php
+++ resources/templates/admin/page.php
@@ -3,7 +3,7 @@
     <h2><?php echo $this->get_page_title(); ?></h2>
     <?php if (!empty($_GET['updated'])) : ?>
         <div id="setting-error-settings_updated" class="updated settings-error notice is-dismissible">
-            <p><strong><?php echo $this->translate('settings_saved'); ?></strong></p><button type="button" class="notice-dismiss"><span class="screen-reader-text"><?php $this->translate('dismiss_notice'); ?></span></button>
+            <p><strong><?php echo $this->translate('settings_saved'); ?></strong></p><button type="button" class="notice-dismiss"><span class="screen-reader-text"><?php echo $this->translate('dismiss_notice'); ?></span></button>
         </div>
     <?php endif; ?>
     <form action="<?php echo $this->get_form_url(); ?>" method="POST">
